3e2d1a34c53385bc95d63bfec8f2267612d53bb1
tshadwel <tshadwel@amazon.com>
2019-07-04 18:22:51 +0100

    seems to be working nicely now

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ const { join } = require('path')
 const readFile = promisify(require('fs').readFile);
 const writeFile = promisify(require('fs').writeFile);
 const exec = promisify(require('child_process').exec);
-const execFile = promisify(require('child_process').exec);
+const { execFile } = require('child_process');
+const ncp = promisify(require('ncp').ncp)
 
 const templateDir = 'create-react-app-z-template';
+const myName = 'create-react-app-z'
 
 
 const noCompleter = () => [];
@@ -36,41 +38,77 @@ const sentencify = ([firstChr, ...etc]) => `${firstChr.toUpperCase()}${etc.join(
 const debug = process.env.DEVELOPMENT
 
 const loadify = (promise, caption) => async (...args) => {
-  const spinner = ora(caption)
+  const spinner = ora(caption).start()
   try {
     const ret = await promise(...args);
     spinner.succeed()
     return ret
   } catch (e) {
-    fail()
-
+    spinner.fail()
     throw (e)
   }
 }
 
-const shell = async (commandLine) => {
-  const { err, stdout, stderr } = await exec(commandLine);
-  if (debug) {
-    console.log(commandLine, stdout, stderr)
-  }
-  if (err) throw err;
-  return stdout.trim();
-}
+class wd extends String {
+  constructor(...args) { super(...args); }
 
-const run = async(program, argv, options) => {
-  const { err, stdout, stderr } = await execFile(program, argv, options);
-  if (err) throw err;
-  return stdout.trim();
+  cd(dir) { return new wd(join(this.toString(), dir)) }
+
+  async cp(target) {
+    const source = this.toString();
+    await loadify(ncp, `cp ${source} ${target}`)(source, target)
+    return new wd(target)
+  }
+
+  async read(file, ...etc) {
+    file = join(this.toString(), file)
+    return await loadify(readFile, `read ${file}`)(file, ...etc)
+  }
+
+  async write(file, ...etc) {
+    file = join(this.toString(), file)
+    return await loadify(writeFile, `write ${file}`)(file, ...etc)
+  }
+
+  async run(program, ...args) {
+    return new Promise((ok, fail) => {
+      const cli_text = [program, ...args].join(" ");
+      const spinner = ora(cli_text).start();
+
+      const process = execFile(program, args, { cwd: this.toString() },
+        (err, stdout, stderr) => {
+          if (err) {
+            spinner.fail(cli_text);
+            fail(err);
+          }
+          spinner.succeed(cli_text);
+          ok(stdout.trim());
+        })
+
+      process.stdout.on('data', d => spinner.text = `[${program} ${this.toString()}] ${d}`);
+    })
+  }
+
+  async _run(program, ...args)  {
+    const { err, stdout, stderr } = await spawn(program, args,  {
+      cwd: this.toString()
+    })
+
+    if (err) throw new Error(`${[program, ...args].join(" ")} failed with ${err}\n\tstdout: ${stdout}\n\tstderr: ${stderr}`);
+    return stdout.trim();
+  }
+
 }
 
 const spaceArray = (s) => s.split(/s+/).map(v => v.trim())
 
+const root = new wd(".")
 const doOptions = async () => {
-  const gitUsername = await shell("git config user.name")
+  const gitUsername = await root.run("git", "config", "user.name")
 
   let options = commander
     .option('--name <name>', 'package name')
-    .option('--version <version>', 'package version', '0.1.0')
+    .option('--ver <version>', 'package version', '0.1.0')
     .option('--description <description>', 'package description')
     .option('--repository <repo uri>', 'package repository URI')
     .option('--author <name>', 'package author name', gitUsername)
@@ -105,39 +143,42 @@ const doOptions = async () => {
   return options
 }
 
+
 async function Do() {
   const { name, version, description, repository,
     author, deps, peers, devDeps } = await doOptions();
 
-  const x = async (cmd) => loadify(shell, cmd)(cmd);
-  const xf = async (...args) => loadify(run, args.join(" "))(...args);
-  const read = async (file, ...args) => await loadify(readFile, `read ${file}`)
-    (name, ...args)
 
-  const write = async (file, ...args) => await loadify(writeFile, `write ${file}`)
-    (file, ...args)
+  const tmpDirName = "tmp-template"
+  await root.run("git", "clone", templateDir, tmpDirName)
+  const tmplDir = root.cd(tmpDirName)
 
-  const pkg = JSON.parse(await read(join(templateDir, 'package.json')))
+  const pkg = JSON.parse(await tmplDir.read("package.json"))
+
   const newPkg = { ...pkg, name, version, description, repository, author };
-  await write(
-    join(templateDir, 'package.json'),
-    JSON.stringify(newPkg, null, 2)
-  )
 
-  if(deps) await xf("yarn", ["add", ...deps], { cwd: templateDir });
-  if(peers) await xf("yarn", ["add", "--peer", "--dev", ...peers], { cwd: templateDir });
-  if(devDeps) await xf("yarn", ["add", "--dev", ...devDeps], { cwd: templateDir });
+  await tmplDir.write('package.json', JSON.stringify(newPkg, null, 2))
+  await tmplDir.run("git", "add", "package.json")
+  await tmplDir.run("git", "commit", "-m", `[${myName}]: update package.json`)
 
-  await xf("yarn", [], {cwd: templateDir})
+  if (deps) await tmplDir.run("yarn", "add", ...deps);
+  if (peers) await tmplDir.run("yarn", "add", "--peer", "--dev", ...peers);
+  if (devDeps) await tmplDir.run("yarn", "add", "--dev", ...devDeps);
 
+
+  await tmplDir.run("yarn")
 
 }
+
 
 const always = () => rl.close();
 
 async function main() {
   await Do();
-  always();
+
 }
 
-main();
+main().catch(e => {
+  always();
+  console.log(e)
+}).then(() => always());
